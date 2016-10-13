90bd5cf00221bcb73c59bc6460d5ea05fb1bf627
Ignacio Losiggio <iglosiggio@gmail.com>
2016-10-13 14:59:10 -0300

    El usando una call de stacktrace-js más retrocompatible

--- debug.js
+++ debug.js
@@ -1,5 +1,5 @@
 var enabled = require('./config.js').DEBUG;
-var stack = require('stacktrace-js');
+var stacktrace = require('stacktrace-js');
 var format = require('util').format;
 var basename = require('path').basename;
 
@@ -11,25 +11,21 @@ if(enabled) {
 		function logger() {
 			var now = (new Date()).toISOString();
 			var args = Array.prototype.slice.call(arguments);
+			var stack = stacktrace.getSync();
+			var log_format = "[%s/%s/%s %s:%s] %s";
+			var caller = stack[1]; /* stack[0] es logger */
+			var f = caller.functionName;
+			var file = basename(caller.fileName); /* Muestro únicamente el nombre del archivo */
+			var line = caller.lineNumber;
+			var args_format = args[0];
+			var args_args = args.slice(1);
+			var string = format(log_format, now, namespace, f, file, line, args_format);
 
-			function log_this(stack) {
-				var log_format = "[%s/%s/%s %s:%s] %s";
-				var caller = stack[1]; /* stack[0] es logger */
-				var f = caller.functionName;
-				var file = basename(caller.fileName); /* Muestro únicamente el nombre del archivo */
-				var line = caller.lineNumber;
-				var args_format = args[0];
-				var args_args = args.slice(1);
-				var string = format(log_format, now, namespace, f, file, line, args_format);
-
-				if(args_args.length > 0) {
-					console.log.apply(console, [string].concat(args_args));
-				} else {
-					console.log(string);
-				}
+			if(args_args.length > 0) {
+				console.log.apply(console, [string].concat(args_args));
+			} else {
+				console.log(string);
 			}
-
-			stack.get().then(log_this);
 		}
 
 		return logger;
